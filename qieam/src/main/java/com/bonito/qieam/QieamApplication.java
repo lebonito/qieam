3a412de33cac7d3e27cc941c45eb7657bb1c8d65
lebonito <dongmochristian@yahoo.fr>
2023-07-23 19:04:55 +0200

    Add files via upload
    Création du projet

---
+++ qieam/src/main/java/com/bonito/qieam/QieamApplication.java
@@ -0,0 +1,13 @@
+package com.bonito.qieam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QieamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QieamApplication.class, args);
+	}
+
+}
---
+++ qieam/src/test/java/com/bonito/qieam/QieamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bonito.qieam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QieamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
