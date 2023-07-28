d3d6248a08b94b45701a751846c96297df92c421
bonito <dongmochristian@yahoo.fr>
2023-07-28 14:55:31 +0200

    search

--- src/main/java/com/bonito/qieam/search/BinarySearch.java
+++ src/main/java/com/bonito/qieam/search/BinarySearch.java
@@ -1,20 +1,51 @@
 package com.bonito.qieam.search;
 
+import java.util.Arrays;
+
 public class BinarySearch {
 
-    int binarySearch(int[] arr, int l, int r, int x) {
-        if (r >= l) {
-            int mid = l + (r - l) / 2;
-            if (arr[mid] == x) {
-                return mid;
-            }
-
-            if (arr[mid] > x) {
-                return binarySearch(arr, l, mid - 1, x);
-            } else {
-                return binarySearch(arr, mid + 1, r, x);
+    int search(int[] arr, int x) {
+        for (int i = 0; i<arr.length; i++) {
+            if (arr[i] == x) {
+                return i;
             }
         }
         return -1;
+    }
+
+    int binarySearch(int[] arr, int x) {
+        int l = 0;
+        int r = arr.length - 1;
+        int[] array = Arrays.stream(arr).sorted().toArray();
+        int mid = arr.length / 2;
+
+        while (r >= l) {
+            if (array[mid] == x) {
+                return mid;
+            }
+            if (array[mid] < x) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    int rebinarySearch(int[] arr, int x) {
+        int[] array = Arrays.stream(arr).sorted().toArray();
+        int mid = arr.length / 2;
+
+        if (array[mid] == x) {
+            return mid;
+        }
+
+        if (array[mid] > x) {
+            int[] ints = Arrays.copyOf(array, mid);
+            return rebinarySearch(ints,  x);
+        } else {
+            int[] ints = Arrays.copyOf(array, mid-1);
+            return rebinarySearch(ints, x);
+        }
     }
 }
